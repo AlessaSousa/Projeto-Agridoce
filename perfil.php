62901816957fa5b2ce032da84bed61cc98531521
Alessandra <alessaconceicao501501@gmail.com>
2024-05-14 18:02:44 -0400

    front atualizado

--- perfil.php
+++ perfil.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AGRIDOCE</title>
-    <link rel="stylesheet" href="style/perfilStyle.css">
+    <link rel="stylesheet" href="style/perfil.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Abel&display=swap" rel="stylesheet">
@@ -17,10 +17,19 @@
     <link href="https://fonts.googleapis.com/css2?family=Abel&family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Kelly+Slab&display=swap" rel="stylesheet">
 </head>
 <body>
-<div class="logo"><a href="index.php">AGRIDOCE</a></div>
-    <div class="nav">
-        <div class="right-links">
-
+   <div class="box-left">
+    
+   </div>
+   <header>PERFIL</header>
+    <div class="perfil-foto">
+        <input type="file" id="perfilFotoInput" accept="image/*" style="display: none;">
+        <label for="perfilFotoInput" class="perfil-foto-label"><svg class='svg' xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-image" viewBox="0 0 16 16">
+  <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
+  <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1z"/>
+</svg></label>
+    </div>
+   <div class="content">
+   <div class="logo"><a href="feed.php">AGRIDOCE</a></div>
             <?php            
             require("conexao.php");
             session_start();
@@ -33,20 +42,22 @@
                 $email = "E-mail não disponível";
             }
             ?>
-            <a href = "sair.php"> <button class="btn">Sair</button></a>
+            <a href = "sair.php"> <button class="btn"><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"/>
+            <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z"/>
+            </svg></button></a>
         </div>
-    </div>
 
     <main>
         <div class="main-box top">
             <div class ="top">
+                <label>Nome</label>
                 <div class="box">
-                Nome
                 <?php echo $nome ?>
             </div>
-
+            <label>E-mail</label>
             <div class="box">
-                E-mail
+
                 <?php echo $email ?>
             </div>
         </div>
