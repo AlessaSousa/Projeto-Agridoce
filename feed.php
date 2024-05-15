f87364972f2f4333fe597444600e82607887b0b7
Alessandra <alessaconceicao501501@gmail.com>
2024-05-15 13:58:35 -0400

    falta implementar o back-end

--- feed.php
+++ feed.php
@@ -17,7 +17,95 @@
     <link href="https://fonts.googleapis.com/css2?family=Abel&family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Kelly+Slab&display=swap" rel="stylesheet">
 </head>
 <body>
-        <div class="logo"><a href="login.php">AGRIDOCE</a></div>
-        
+    <div class="box-top">
+
+    </div>
+
+<div class="logo"><a href="login.php">AGRIDOCE</a></div>
+       
+        <div class="busca">
+        <input type="text" id="busca" placeholder="Buscar receitas...">
+        <button onclick="pesquisar()"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+        </svg></button>
+    </div>  
+
+     <div class="icon-perfil">
+            <a href ="perfil.php">
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+         <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+         <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+        </svg></a>
+        </div>        
+        <header>Feed de Receitas</header>  
+        <div class="container">
+
+        <div class="card" onclick="abrirReceita('#')">
+        <svg class='svg' xmlns="http://www.w3.org/2000/svg" width="200" height="200" fill="currentColor" class="bi bi-image" viewBox="0 0 16 16">
+            <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
+            <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1z"/>
+            </svg>
+            <img src="" >
+            <h2>Bolo de Chocolate</h2>
+        </div>
+
+        <div class="card" onclick="abrirReceita('#')">
+        <svg class='svg' xmlns="http://www.w3.org/2000/svg" width="200" height="200" fill="currentColor" class="bi bi-image" viewBox="0 0 16 16">
+            <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
+            <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1z"/>
+            </svg>
+            <img src="" >
+            <h2>Mousse de Maracujá</h2>
+        </div>
+
+            <div class="card" onclick="abrirReceita('#')">
+        <svg class='svg' xmlns="http://www.w3.org/2000/svg" width="200" height="200" fill="currentColor" class="bi bi-image" viewBox="0 0 16 16">
+            <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
+            <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1z"/>
+            </svg>
+            <img src="" >
+            <h2>X-Caboquinho</h2>
+        </div>
+
+            <div class="card" onclick="abrirReceita('#')">
+        <svg class='svg' xmlns="http://www.w3.org/2000/svg" width="200" height="200" fill="currentColor" class="bi bi-image" viewBox="0 0 16 16">
+            <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
+            <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1z"/>
+            </svg>
+            <img src="" >
+            <h2>Sopa de legumes</h2>
+        <!-- Adicione mais cards conforme necessário -->
+    </div>
+
+    <script>
+        function abrirReceita(url) {
+            window.location.href = url;
+        }
+        <?php
+          include "conexao.php";
+          $dest = "SELECT * FROM receita WHERE recnome, recingrediente";
+          $result = mysqli_query($con, $dest);                
+              while ($r = mysqli_fetch_array($result)) {
+                $reccod = $r['reccod'];
+                $recnome = $r['recnome'];
+          ?>
+    </script>
+        <div class="container">
+        <div class="card">
+          <h2> <?php echo $r['recnome'] ?></h2>
+          <img src="<?php echo $r['link'] ?>" alt="cursovideophp" width="320" height="200"/>
+          <p style="text-align: justify;" class="card-text"><?php echo $r['recingrediente'] ?></p>
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="btn-group">
+            <?php echo '<a href="post.php?receita='.$recnome.'&id='.$reccod.'';?>" ><button type="button" class="btn">Visualizar</button></a>
+            <a href="perfil.php"><button type="button" class="btn btn">Inscreva-se</button></a>
+             
+            </div>
+        </div>
+      </div>
+    </div>
+    <?php    
+                 }
+              ?>
 </body>
 </html>
--- perfil.php
+++ perfil.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AGRIDOCE</title>
-    <link rel="stylesheet" href="style/perfil.css">
+    <link rel="stylesheet" href="style/perfilStyle.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Abel&display=swap" rel="stylesheet">
@@ -20,26 +20,25 @@
    <div class="box-left">
     
    </div>
-   <header>PERFIL</header>
+   <header>PERFIL</header><!--
     <div class="perfil-foto">
         <input type="file" id="perfilFotoInput" accept="image/*" style="display: none;">
-        <label for="perfilFotoInput" class="perfil-foto-label"><svg class='svg' xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-image" viewBox="0 0 16 16">
-  <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
-  <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1z"/>
-</svg></label>
-    </div>
+        <label for="perfilFotoInput" class="perfil-foto-label">
+            <svg class='svg' xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-image" viewBox="0 0 16 16">
+            <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
+            <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1z"/>
+            </svg></label>
+                </div>-->
    <div class="content">
    <div class="logo"><a href="feed.php">AGRIDOCE</a></div>
             <?php            
             require("conexao.php");
             session_start();
-            if(isset($_SESSION['$nome']) && isset($_SESSION['email'])){
+            if(isset($_SESSION['$nome'])){
                 $nome = $_SESSION['nome'];
-                $email = $_SESSION['email'];
 
             }else{
                 $nome = "Nome não disponível";
-                $email = "E-mail não disponível";
             }
             ?>
             <a href = "sair.php"> <button class="btn"><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
@@ -51,17 +50,38 @@
     <main>
         <div class="main-box top">
             <div class ="top">
-                <label>Nome</label>
+                <p>Olá</p>
                 <div class="box">
                 <?php echo $nome ?>
             </div>
-            <label>E-mail</label>
-            <div class="box">
+        </div>
+        </div>
 
-                <?php echo $email ?>
-            </div>
+
+    <div class="container-p">
+        <div class="box-p form-box-p">
+        <h2>Publique sua receita</h2><br>
+        <form action="" method="POST">
+        <div class="field-p input-p">
+            <input type="text" id="inserir-receita" placeholder="Digite sua receita... '" oninput="resizeInput()">
         </div>
+        <div class="field-p">
+            <button class="btn-login" id="btn" title="Adicione">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-check-square" viewBox="0 0 16 16">
+                <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                <path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"/>
+                </svg>
+            </button>
+      </div>
         </div>
-    </main>
+      </form>
+      <script>
+        function resizeInput() {
+            var input = document.getElementById('inserir-receita');
+            input.style.height = (input.value.length + 1) * 10 + 'px';
+        }
+    </script>
+ 
+    </div>     </main>
 </body>
 </html>
